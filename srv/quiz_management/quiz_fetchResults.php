3691844fc1c1f7ea21e2afc99bb95cfc0cd1b1bf
Markus Kluge <m.kluge1@gmx.de>
2018-06-30 23:52:02 +0200

    removed log to local file

--- srv/quiz_management/quiz_fetchResults.php
+++ srv/quiz_management/quiz_fetchResults.php
@@ -4,7 +4,6 @@ require_once('../Quiz.php');
 session_start();
 
 $results = $_SESSION['quiz']->getResults();
-file_put_contents ( "c:/temp/log.txt" , print_r( $results ,true) );
 
 echo json_encode( $results );
 // echo json_encode( "php answer" );
